22edf1938319e43bf59abc30db3257983a5a28cc
Oleg <oleg.marinin.49@yandex.ru>
2025-03-28 16:00:00 +0300

    security configuration

---
+++ user_service/src/main/java/ru/marinin/user_service/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package ru.marinin.user_service.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="asdhfiasugh9rehgvoqevqieufhnvuqehrguqpodsfjasdjfaoiuwrg";
+    public static final String JWT_HEADER = "Authorization";
+}
---
+++ user_service/src/main/java/ru/marinin/user_service/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package ru.marinin.user_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.marinin.user_service.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByEmail(String email);
+}
