829711d0a4b465601596d5c32d7345943022919e
Oleg <oleg.marinin.49@yandex.ru>
2025-03-28 07:17:00 +0300

    security configuration

---
+++ user_service/src/main/java/ru/marinin/user_service/config/ApplicationConfiguration.java
@@ -0,0 +1,61 @@
+package ru.marinin.user_service.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.hibernate.annotations.EmbeddableInstantiatorRegistrations;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class ApplicationConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.sessionManagement(
+                managment -> managment.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS
+                )
+        ).authorizeHttpRequests(
+                Authorize -> Authorize.requestMatchers("/api/**")
+                        .authenticated()
+                        .anyRequest()
+                        .permitAll()
+        ).addFilterBefore(new JwtTokenValidator, BasicAuthenticationFilter.class)
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(CorsConfigurationSource()))
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+    private CorsConfigurationSource CorsConfigurationSource() {
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration cfg = new CorsConfiguration();
+                cfg.setAllowedOrigins(Collections.singletonList("*"));
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setAllowCredentials(true);
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                cfg.setExposedHeaders(Arrays.asList("Authorization"));
+                cfg.setMaxAge(3600L);
+                return cfg;
+            }
+        };
+    }
+
+    public PasswordEncoder passwordEncoder() {
+
+    }
+}
