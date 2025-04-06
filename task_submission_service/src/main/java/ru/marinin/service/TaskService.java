226d9b247285c27c750fdf1082b7f84af75d5945
Oleg <oleg.marinin.49@yandex.ru>
2025-04-06 20:09:10 +0300

    feign client models

--- task_submission_service/src/main/java/ru/marinin/service/TaskService.java
+++ task_submission_service/src/main/java/ru/marinin/service/TaskService.java
@@ -5,13 +5,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import ru.marinin.model.TaskDto;
 
 @FeignClient(name = "SUBMISSION-SERVICE", url = "http://localhost:5003/")
 public interface TaskService {
 
-    @GetMapping("/{id}")
+    @GetMapping("/api/tasks/{id}")
     public ResponseEntity<TaskDto> getTaskById(
+            @PathVariable Long id,
+            @RequestHeader("Authorization") String jwt) throws Exception;
+
+    @PutMapping("/api/tasks/{id}/complete")
+    public ResponseEntity<TaskDto> completeTask(
             @PathVariable Long id) throws Exception;
 
 }
