247a0d7e684d0eb983ec2fccad1929c0af7c56a5
Oleg <oleg.marinin.49@yandex.ru>
2025-04-20 13:28:36 +0300

    front baseUrl to api gateway url

--- frontend_v2/src/services/api.js
+++ frontend_v2/src/services/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001',
-  withCredentials: true, // Базовый URL вашего бэкенда
+  baseURL: 'http://localhost:5000',
+  withCredentials: true,
   headers: {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest'
   }
 });
 
