ad2e28c1f6cd77d3d94eb5ecc5cc30974b403fda
Oleg <oleg.marinin.49@yandex.ru>
2025-07-12 09:54:34 +0300

    backend with monolithic architecture initial commit

---
+++ backend_monolithic/src/main/java/backend_monolithic/BackendMonolithicApplication.java
@@ -0,0 +1,13 @@
+package backend_monolithic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMonolithicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMonolithicApplication.class, args);
+	}
+
+}
---
+++ backend_monolithic/src/test/java/backend_monolithic/BackendMonolithicApplicationTests.java
@@ -0,0 +1,13 @@
+package backend_monolithic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMonolithicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
