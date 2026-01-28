87dda0ec3de3facd3648bf1fcf9a08ff84b50836
Oleg <oleg.marinin.49@yandex.ru>
2026-01-28 23:50:42 +0300

    fix auth error

--- backend_monolithic/src/main/java/backend_monolithic/controller/AuthController.java
+++ backend_monolithic/src/main/java/backend_monolithic/controller/AuthController.java
@@ -27,7 +27,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     private final UserRepository userRepository;
